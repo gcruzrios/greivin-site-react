05c35bf2fb5274d3a49f31a0fd364619cd9d5f3b
gcruzrios <gcruzr@gmail.com>
2023-08-11 17:38:19 -0600

    site completed-v1.3

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import {
   BrowserRouter as Router,
   Route,
@@ -10,54 +10,50 @@ import {
 
 import axios from "axios";
 const Detail = () => {
-    const [data, setData] = useState([]);
-    const [title, setTitle] = useState([]);
-    const { id } = useParams();
-    function removeTags(str) {
-      if (str === null || str === "") return false;
-      else str = str.toString();
-  
-      // Regular expression to identify HTML tags in
-      // the input string. Replacing the identified
-      // HTML tag with a null string.
-      return str.replace(/(<([^>]+)>)/gi, "");
-    }
-  
-    const peticionGet = async () => {
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+  const { id } = useParams();
+  function removeTags(str) {
+    if (str === null || str === "") return false;
+    else str = str.toString();
 
-     
-      await axios.get(`/wp-json/wp/v2/posts/${id}`).then((response) => {
-        
-        setTitle(response.data.title.rendered);
-        setData(response.data.content.rendered);
-        console.log(data);
+    // Regular expression to identify HTML tags in
+    // the input string. Replacing the identified
+    // HTML tag with a null string.
+    return str.replace(/(<([^>]+)>)/gi, "");
+  }
 
-      });
-    };
-  
-    useEffect(() => {
-      peticionGet();
-    }, []);
-    return (
-      <div>
-        <Header />
-        <br/>
-          <h2>
+  const peticionGet = async () => {
+    await axios.get(`/wp-json/wp/v2/posts/${id}`).then((response) => {
+      setTitle(response.data.title.rendered);
+      setData(response.data.content.rendered);
+      console.log(data);
+    });
+  };
+
+  useEffect(() => {
+    peticionGet();
+  }, []);
+  return (
+    <div>
+      <Header />
+      <br />
+
+      {/* <!-- ======= Resume Section ======= --> */}
+      <section id="resume" className="resume">
+        <div className="container">
+          <h2 className="text-end">
             <Link to="/news">Go to News Page</Link>
           </h2>
-        {/* <!-- ======= Resume Section ======= --> */}
-        <section id="resume" className="resume">
-          <div className="container">
-            <div className="section-title">
-                <h2>{title}</h2>  
-            </div>
-             <div dangerouslySetInnerHTML={{ __html: data }} /> 
+          <div className="section-title">
+            <h2>{title}</h2>
           </div>
-        </section>
-        {/* <!-- End Resume Section --> */}
-      </div>
-    );
-  };
-  
-  export default Detail;
-  
+          <div dangerouslySetInnerHTML={{ __html: data }} />
+        </div>
+      </section>
+      {/* <!-- End Resume Section --> */}
+    </div>
+  );
+};
+
+export default Detail;
